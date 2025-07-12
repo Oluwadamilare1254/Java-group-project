a84543da8e58a32efbc2e95e39b46c460cd3f490
geniusspiff <br4despiff@gmail.com>
2025-07-13 00:34:20 +0100

    added gamestate.java

--- gamestate.java
+++ gamestate.java
@@ -1,1 +1,15 @@
-g
+package ui;
+
+public class GameState {
+    public static String playerOneName = "";
+    public static String playerTwoName = "";
+    public static int playerOneScore = 0;
+    public static int playerTwoScore = 0;
+    public static int draws = 0;
+    public static Object playerOne;
+    public static Object playerTwo;
+
+
+    public void setPlayerNames(String playerOne, String playerTwo) {
+    }
+}
